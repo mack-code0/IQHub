d4d98c7c1bc0191bfa013d288576fb551b93d18f
mack-code0 <macaulayhilbert202@gmail.com>
2022-02-27 04:52:06 +0800

    fourth commit

--- public/javascript/add-subject.js
+++ public/javascript/add-subject.js
@@ -1,20 +1,26 @@
+// Button for creating new subject tab
 let addSubjectBtn = document.querySelector(".add_subject .button")
-let subject_name = document.querySelector(".add_subject input")
 
+// Input tag holding the name of the subject tab about to be added
+let subject_name = document.querySelector(".add_subject input")
+const all_subject_names = []
 
 
 
 addSubjectBtn.addEventListener("click", ()=>{
+    // Removing whitespaces from the subjact tab name so it can be added as an id
     let real_subject_name = subject_name.value.replace(/\s/g, "")
+    all_subject_names.push(real_subject_name)
+    // creating the fields to accept the question and options of a particaular aubject tab using it's id
     var htm = `<button class="button dark_bg white_txt px-2 py-1 me-2 d-flex align-items-center"><i class="bx bx-trash me-1"></i>
-    <a onclick="clickedSubjectName(this)" data-bs-toggle="collapse"  href="#${real_subject_name}id" role="button" aria-expanded="false" aria-controls="${real_subject_name}id">${subject_name.value}</a>
-</button>`
+        <a onclick="clickedSubjectName(this)" data-bs-toggle="collapse"  href="#${real_subject_name}id" role="button" aria-expanded="false" aria-controls="${real_subject_name}id">${subject_name.value}</a>
+    </button>`
 	$(".exam_subject_names").append(htm);
 
     var newHtml = `<div id="${real_subject_name}id" class="form_1 w-100 mt-5 collapse multi-collapse">
     <h6 class="thick mb-4">${subject_name.value}</h6>
     <div class="question mb-5 w-100">
-        <h6 class="thick me-3">1.</h6>
+        <h6 class="thick me-3 ${real_subject_name}id_current_number">1.</h6>
         <div class="question_options w-100">
             <textarea name="" id="${real_subject_name}id_main_question" class="w-100 mb-2" rows="4"></textarea>
             <div class="options w-100 d-flex flex-wrap">
@@ -35,24 +41,45 @@ addSubjectBtn.addEventListener("click", ()=>{
                     <input type="text" id="option_d" class="w-100 ${real_subject_name}id_options">
                 </div>
             </div>
+            </div>
         </div>
-    </div>
-</div>`
+    </div>`
 
     $(".all_question_to_add").append(newHtml);
     subject_name.value = ""
+    document.querySelector(".add_subject button").setAttribute("disabled", "true")
 })
 
 
+// Function for the subject tab button
 let clickedSubjectName = (m)=>{
-    document.querySelector(".add_question").setAttribute("id", `${m.getAttribute("href").substring(1)}`)
+    // When the function is clicked, the button for creating new question is assigned a new id with using the name of that particular tab clicked
+    document.querySelector(".add_question").setAttribute("id", `${m.getAttribute("href").substring(1)}_btn`)
     let subject_name_btn_array = document.querySelectorAll(".exam_subject_names button a")
     let all_question_to_add_array = document.querySelector(".all_question_to_add").childNodes
+    
+    // Looping through the tabs to remove the tabs that were'nt clicked on from the view
     for(let i = 0; i<subject_name_btn_array.length; i++){
         if(subject_name_btn_array[i].getAttribute("href").substring(1) !== m.getAttribute("href").substring(1)){
             subject_name_btn_array[i].setAttribute("aria-expanded", "false");
             subject_name_btn_array[i].setAttribute("class", "collapsed");
             all_question_to_add_array[i].classList.remove("show")
         }
+    }
+}
+
+
+
+const format = /[`!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/
+const checkInput = (m)=>{
+    console.log(all_subject_names);
+    if(m.value.length < 3 || m.value == "" || format.test(m.value)){
+        document.querySelector(".add_subject button").setAttribute("disabled", "true")
+    }
+    else if(all_subject_names.indexOf(m.value)>-1){
+        document.querySelector(".add_subject button").setAttribute("disabled", "true")
+    }
+    else{
+        document.querySelector(".add_subject button").removeAttribute("disabled")
     }
 }
--- public/javascript/myscript.js
+++ public/javascript/myscript.js
@@ -1,23 +1,76 @@
-let add_btn = document.querySelector(".add_question")
+// Button for adding question to question json object
+const add_btn = document.querySelector(".add_question")
 
-let all_questions_holder = {}
+// Holds all the questions in a json formmat/object format
+const all_questions_holder = {}
 
 add_btn.addEventListener("click", function(){
-    let option_holder = []
+    // The id to the current active tab gotten from the id of add_btn
+    const question_tab_id  = this.id.split("_")[0]
 
-    console.log(`${this.id}`);
-    let options = this.parentElement.parentElement.querySelectorAll(`.all_question_to_add #${this.id} .question .question_options .options div input`);
-    let main_question = this.parentElement.parentElement.querySelector(`.all_question_to_add #${this.id} .question .question_options textarea`).value
+    
+    // Options of the question
+    const options = document.querySelectorAll(`#${question_tab_id} .question .question_options .options div input`);
+    // Question about to be added
+    const main_question = document.querySelector(`#${question_tab_id} .question .question_options textarea`)
+    // Array to hold the options after looping through them
+    const option_holder = []
     options.forEach(inp=>{
         option_holder.push(inp.value)
     })
-    let all_holder;
+
+    const next_question_number = document.querySelector(`.${question_tab_id}_current_number`)
+
+    // Check if the tab this question belongs to is already in the object holding all the questions
+    // If the tab is already present, then update it by pushing the question to the tab it belongs, else create the tab and assign the new question to the tab
     if(all_questions_holder[`${this.id}`]){
-        all_questions_holder[`${this.id}`].push({question: main_question, options: option_holder})
+        const question_model = {question: main_question.value, options: option_holder}
+        all_questions_holder[`${this.id}`].push(question_model)
+        // Adding new question to the view
+        add_new_question(question_model, all_questions_holder[`${this.id}`].length, question_tab_id)
+        next_question_number.innerHTML = all_questions_holder[`${this.id}`].length + 1 +"."
     }else{
-        all_holder = [{question: main_question, options: option_holder}]
-        all_questions_holder[`${this.id}`] = all_holder
+        all_questions_holder[`${this.id}`] = [{question: main_question.value, options: option_holder}]
+        // Adding new question to the view
+        add_new_question(all_questions_holder[`${this.id}`][0], 1, question_tab_id)
+        next_question_number.innerHTML = 2 + "."
     }
 
-    console.log(all_questions_holder);
+    // Set the input values to none
+    main_question.value = ""
+    options.forEach(inp=>{
+        inp.value = ""
+    })
 })
+
+
+
+
+const add_new_question = (question, question_index, question_tab_id)=>{
+    const newly_added_question = `<div class="mb-5 w-100 added_questions">
+        <span class="thick">${question_index}.</span>
+        <h6 class="thick mt-2">${question.question}</h6>
+        <div class="d-flex flex-wrap">
+            <div class="w-50 mb-1 pe-3">
+                <span class="thick">A.</span>
+                <h6 class="less_thick">${question.options[0]}</h6>
+            </div>
+            <div class="w-50 mb-1 ps-3">
+                <span class="thick">C.</span>
+                <h6 class="less_thick">${question.options[1]}</h6>
+            </div>
+            <div class="w-50 mt-1 pe-3">
+                <span class="thick">B.</span>
+                <h6 class="less_thick">${question.options[2]}</h6>
+            </div>
+            <div class="w-50 mt-1 ps-3">
+                <span class="thick">D.</span>
+                <h6 class="less_thick">${question.options[3]}</h6>
+            </div>
+        </div>
+    </div>`
+    $(`#${question_tab_id}`).append(newly_added_question);
+}
+
+
+
